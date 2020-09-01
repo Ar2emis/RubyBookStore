4b6b94cfefd5046c7b0fe9656e54f9f8588594af
Artyom Shevchenko <artemsheva0510@gmail.com>
2020-09-01 12:51:59 +0300

    remove books and cartitems routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root 'home#index'
-
-  resources :cart_items, only: [:create]
-  resources :books, only: [:index]
 end
