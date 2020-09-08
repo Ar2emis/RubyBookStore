1e7cb9ab7ef55c5fb281ef095caf7b0d89de4d38
Artyom Shevchenko <artemsheva0510@gmail.com>
2020-09-08 21:47:05 +0300

    add query object LatestBooks

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,6 @@ class HomeController < ApplicationController
   LATEST_BOOKS_AMOUNT = 3
 
   def index
-    @latest_books = BookDecorator.decorate_collection(latest_books)
-  end
-
-  private
-
-  def latest_books
-    Book.with_authors.last(LATEST_BOOKS_AMOUNT)
+    @latest_books = LatestBooks.new(LATEST_BOOKS_AMOUNT).query.decorate
   end
 end
