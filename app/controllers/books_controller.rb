ff6691dd62dadadf9a64ac28af3fd2a19bf80111
Artyom Shevchenko <artemsheva0510@gmail.com>
2020-09-09 16:23:53 +0300

    trying to implement pagy countless

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,25 @@
 class BooksController < ApplicationController
+  include Pagy::Backend
   include Rectify::ControllerHelpers
   BOOKS_AMOUNT = 12
 
-  def index
-    present BooksPresenter.new(books: prepared_books, categories: @categories)
-  end
+  # def index
+  #   present BooksPresenter.new(books: prepared_books, categories: @categories)
+  # end
 
   def show
     present BookPresenter.new(book: Book.find(book_param).decorate)
   end
 
+  def index
+    @pagy, books = pagy_countless(prepared_books, link_extra: 'data-remote="true"')
+    present BooksPresenter.new(books: books, categories: @categories)
+  end
+
   private
 
   def prepared_books
-    FilteredAndSortedBooks.new(category: category_param, sort: sort_param, amount: books_amount).query.decorate
+    FilteredAndSortedBooks.new(category: category_param, sort: sort_param).query.decorate
   end
 
   def books_amount
