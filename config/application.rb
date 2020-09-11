fff7cb7687823a26a6383fb758b31b886b5f1ec0
Artyom Shevchenko <artemsheva0510@gmail.com>
2020-09-11 03:41:31 +0300

    - fix error with dotenv

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,6 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-Dotenv::Railtie.load
-
 module BookStore
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
