5ffae0af105034d4de61579ec329cd0b0a2fa7bb
Artyom Shevchenko <artemsheva0510@gmail.com>
2020-10-07 12:47:09 +0300

    refactoring

--- spec/features/devise/registrations/edit_spec.rb
+++ spec/features/devise/registrations/edit_spec.rb
@@ -1,5 +1,5 @@
 RSpec.describe 'Edit', type: :feature do
-  [BillingAddress.to_s, ShippingAddress.to_s].each do |type|
+  %i[billing shipping].each do |type|
     describe type, js: true do
       let(:address_data) { attributes_for(:address) }
       let(:user) { create(:user) }
@@ -11,7 +11,7 @@ RSpec.describe 'Edit', type: :feature do
 
       context 'with valid input' do
         before do
-          within "##{type}" do
+          within "##{type}_address" do
             fill_in(I18n.t('simple_form.placeholders.defaults.first_name'), with: address_data[:first_name])
             fill_in(I18n.t('simple_form.placeholders.defaults.last_name'), with: address_data[:last_name])
             fill_in(I18n.t('simple_form.placeholders.defaults.address'), with: address_data[:address])
@@ -30,7 +30,7 @@ RSpec.describe 'Edit', type: :feature do
 
       context 'with invalid input' do
         before do
-          within "##{type}" do
+          within "##{type}_address" do
             click_button(I18n.t('addresses.save'))
           end
         end
